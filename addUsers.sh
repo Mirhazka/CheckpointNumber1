88d5d002fc6a6ad2c9f99b90d778be51bd5f64f3
sybill <sybillgribonval@gmail.com>
2024-10-26 15:10:08 +0200

    maj fichier addUsers.sh

--- addUsers.sh
+++ addUsers.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-##### Variables #####
-
-
-#####################
-
-##### Fonctions #####
-# Pour chaque utilisateur à créer, il doit y avoir une vérification de l'existence dans le système.
-# S'il existe déjà, un message indiquera "L'utilisateur <nom_utilisateur> existe déjà" et le script continue.
-# Pour chaque utilisateur créer, il doit y avoir une vérification de cette création.
-# Si la création s'est bien effectuée, un message affiche "L'utilisateur <nom_utilisateur> a été crée".
-# Sinon, un message affiche "Erreur à la création de l'utilisateur <nom_utilisateur>". Dans tous les cas, le script continue.
-function addUser () {
-    echo "$(grep $@ /etc/passwd)"
-}
-
-#####################
-
-##### Script #####
-# vérification de la présence d'arguments
-if [ $# != 0 ];then
-    echo "Envoie de la fonction !"
-    addUser
-else
-    echo "Il manque les noms d'utilisateurs en argument - Fin du script."
+# Check if any arguments is provided.
+if [ $# -eq 0 ];then
+    echo "Il manque les noms d'utilisateurs en argument - Fin du script"
     exit 1
 fi
 
-#####################
+# Check if the user exist
+args=$@
+
+for user in $args;
+do
+    if grep -q "^$user:" /etc/passwd; then
+        echo "L'utilisateur $user existe déjà"
+    else
+        if sudo useradd $user; then
+            echo "L'utilisateur $user a été crée."
+        else
+            echo "Erreur à la création de l'utilisateur $user"
+        fi
+    fi
+done
+
+echo "[REMARQUE] Il n'est pas possible de continuer le script sans pour autant de nouveau rentrer des arguments!"
